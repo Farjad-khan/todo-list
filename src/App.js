62e2eefa85b5aee829baa89b09da308e00688b9e
Farjad Ahmed Khan <71921754+Farjad-khan@users.noreply.github.com>
2021-06-18 10:28:38 +0500

    initial commemt

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./index.css";
 
 function App() {
+  const [todo, setTodo] = useState([]);
+  const [value, setValue] = useState("");
+  const [searchValue, setSearchValue] = useState([]);
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setValue(value);
+    const result = todo.filter((name) => name.includes(value));
+    setSearchValue(result);
+  };
+
+  const handleSubmit = () => {
+    const newArray = todo.concat(value);
+    setTodo(newArray);
+    setValue("");
+  };
+
+  const deleteTodo = (index) => {
+    const newTodos = [...todo];
+    newTodos.splice(index, 1);
+    setTodo(newTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <h2>Todo List</h2>
+        <br />
+        <input type="text" value={value} onChange={handleChange} />
+        <button className="todo-btn" onClick={handleSubmit}>
+          Add Todo
+        </button>
+
+        <div className="todo">
+          {value
+            ? searchValue.map((singleTodo, idx) => (
+                <div key={idx}>
+                  <p className="singleTodo">{singleTodo}</p>
+                </div>
+              ))
+            : todo.map((singleTodo, idx) => (
+                <div key={idx}>
+                  <p className="singleTodo">{singleTodo}</p>
+                  <button
+                    className="delete-btn"
+                    onClick={() => {
+                      deleteTodo(idx);
+                    }} 
+                  >
+                    X
+                  </button>
+                </div>
+              ))}
+        </div>
+      </div>
+    </>
   );
 }
 
